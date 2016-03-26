1fd201d835ecf7271768f03b66eda982ec2ffccb
Csaba Marton <martoncsab@gmail.com>
2016-03-26 18:46:53 +0100

    simple rest is working

--- RestClient/ViewController.swift
+++ RestClient/ViewController.swift
@@ -10,8 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingContent: UILabel!
+    @IBOutlet var nameField: UITextField!
+    
+    @IBAction func fetchGreeting(sender: AnyObject) {
+      
+        let postEndpoint: String = "http://localhost:9000/greeting"
+        
+        doRequest(postEndpoint) { data in
+            dispatch_async(dispatch_get_main_queue()) {
+                if let data = data {
+                    do {
+                        let greeting = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                        let content = greeting["content"] as? String
+                        self.greetingContent.text = content
+                        //let id = greeting["id"] as? String
+                        
+                    } catch let error as NSError {
+                        print("Failed to load: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
+    }
+ 
+    func doRequest(link:String,completion: ((data: NSData?) -> Void)) {
+        if let requestUrl = NSURL(string: link){
+            let request = NSMutableURLRequest(URL: requestUrl)
+            
+            request.HTTPMethod = "POST"
+            let postString = "name=" + nameField.text!
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+                completion(data: NSData(data: data!))
+                if let error = error {
+                    print("error=\(error)")
+                    return
+                }
+                
+                }.resume()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
